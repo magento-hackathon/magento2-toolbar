b3dcaf3a19dd7e116444599364959e675421c57e
Barry vd. Heuvel <barryvdh@gmail.com>
2016-05-20 16:07:20 +0200

    Add Base plugin to add collectors

---
+++ Plugin/AbstractAddCollectorPlugin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MagentoHackathon\Toolbar\Plugin;
+
+use MagentoHackathon\Toolbar\Toolbar;
+use DebugBar\DataCollector\DataCollectorInterface;
+
+/**
+ * Plugin to add a collector to the toolbar
+ *
+ * Example usage in etc/di.xml:
+ *
+ * <type name="Magento\Framework\AppInterface">
+ *   <plugin name="MyCollectorPlugin" type="MyCollectorPlugin" sortOrder="0" />
+ * </type>
+ *
+ */
+abstract class AbstractAddCollectorPlugin
+{
+    /** @var  Toolbar */
+    protected $toolbar;
+
+    /** @var DataCollectorInterface  */
+    protected $collector;
+
+    /**
+     * Constructor
+     *
+     * @param Toolbar $toolbar
+     * @param DataCollectorInterface $collector
+     */
+    public function __construct(Toolbar $toolbar, DataCollectorInterface $collector)
+    {
+        $this->toolbar = $toolbar;
+        $this->collector = $collector;
+    }
+
+    /**
+     * Add the collector
+     *
+     */
+    public function beforeLaunch()
+    {
+        if ( ! $this->toolbar->hasCollector($this->collector->getName())) {
+            $this->toolbar->addCollector($this->collector);
+        }
+    }
+}
